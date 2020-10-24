806c9dbb3b1c23cbcd86fc12785bbf8d533016c6
Vivek Singal <viveksingal98@gmail.com>
2020-10-24 15:27:23 +0530

    recipes crud done

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -6,14 +6,8 @@ class RecipesTest < ActionDispatch::IntegrationTest
   # end
   def setup
     @chef = Chef.create!(chefname: "mashrur", email: "mashrur@example.com")
-    @recipe = Recipe.create(name: "vegetable saute",
-
-                            description: "great vegetable sautee,
-
-                                                     add vegetable and oil", chef: @chef)
-    @recipe2 = @chef.recipes.build(name: "chicken saute",
-
-                                   description: "great chicken dish")
+    @recipe = Recipe.create(name: "vegetable saute",description: "great vegetable saute", chef: @chef)
+    @recipe2 = @chef.recipes.build(name: "chicken saute", description: "great chicken dish")
     @recipe2.save
   end
 
@@ -35,6 +29,33 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @recipe.name, response.body
     assert_match @recipe.description, response.body
     assert_match @chef.chefname, response.body
+    assert_select 'a[href=?]', edit_recipe_path(@recipe), text: "Edit this recipe"
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+    assert_select 'a[href=?]', recipes_path, text: "Return to recipes listing"
   end
+
+  test "reject invalid recipe submissions" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: { name: " ", description: " " } }
+    end
+    assert_template 'recipes/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+
+  test "create new valid recipe" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    name_of_recipe = "chicken saute"
+    description_of_recipe = "add chicken, add vegetables, cook for 20 minutes, serve delicious meal"
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: { recipe: { name: name_of_recipe,description: description_of_recipe}}
+    end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe, response.body
+  end  
 
 end
